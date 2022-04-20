6965785fa282e0f7c670a93714110479363e919c
Sania Kousar <102238051+SaniaKousar@users.noreply.github.com>
2022-04-20 11:21:20 +0500

    Create for.loop example 1.cs

---
+++ for.loop example 1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @for.loop_example_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int t;
+            Console.WriteLine("Enter table value ");
+            t = Convert.ToInt32((Console.ReadLine()));
+
+            for (int a = 10; a <= 10; a++)
+            {
+                Console.WriteLine((t) ("+ * ") +a ("+ = ") (+t * a));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
