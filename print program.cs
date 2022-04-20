53e3dfff61bc17318c521a0d6efac88cb7aa5cea
Sania Kousar <102238051+SaniaKousar@users.noreply.github.com>
2022-04-20 08:09:14 +0500

    Create print program.cs

---
+++ print program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
