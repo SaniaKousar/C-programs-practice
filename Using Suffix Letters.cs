6d6a548061e66a0679d4074c7cc428481578dced
Sania Kousar <102238051+SaniaKousar@users.noreply.github.com>
2022-04-24 13:07:47 +0500

    Create Using Suffix Letters.cs

---
+++ Using Suffix Letters.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Suffix_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            uint y = 2U;
+            float f = 20.78F;
+            decimal d = 78.56M;
+            long l = 75L;
+            Console.WriteLine("Value of x is "+x +"\nd is "+d +"\nand l is "+l);
+            Console.ReadLine();
+
+        }
+    }
+}
