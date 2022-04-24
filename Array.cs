3da35c5d980b4a6a1c05b4796266b77411cdf45c
Sania Kousar <102238051+SaniaKousar@users.noreply.github.com>
2022-04-24 13:02:27 +0500

    Create Array.cs

---
+++ Array.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []x= { 8,36,7,0,1,2 };
+            for (int i = 0; i < 6; i++)
+                Console.WriteLine(x[i]);
+            Console.ReadLine();
+        }
+    }
+}
